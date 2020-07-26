291adcdc1dbe335e77e0a9f55c369b4d807fab98
devmatsuko <devmatsuko@gmail.com>
2020-07-26 22:25:43 +0900

    20200726 Play画面のアラート追加

--- AccelerationProphecyMagicApp/Controller/PlayViewController.swift
+++ AccelerationProphecyMagicApp/Controller/PlayViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreMotion
 
 class PlayViewController: UIViewController {
     
@@ -15,22 +16,60 @@ class PlayViewController: UIViewController {
     
     @IBOutlet weak var headLabel: UILabel!
     
-    
     @IBOutlet weak var footLabel: UILabel!
+    
+    // MotionManager
+    let motionManager = CMMotionManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+//        if motionManager.isAccelerometerAvailable {
+//           // intervalの設定 [sec]
+//           motionManager.accelerometerUpdateInterval = 0.2
+//
+//           // センサー値の取得開始
+//           motionManager.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: {(motion:CMDeviceMotion?, error:Error?) in
+//               self.getMotionData(deviceMotion: motion!)
+//
+//           })
+//       }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         playImage.image = UIImage(named: "Trump")
-        
-        
+    
     }
 
-   
+    
+    @IBAction func playStart(_ sender: Any) {
+        
+        //アラートの作成
+        let dialog = UIAlertController(title: nil, message: "スマートフォンの画面が下向きになるように置いてください。", preferredStyle: .alert)
+        //ボタンのタイトル
+        dialog.addAction(UIAlertAction(title: "Return to Top", style: .default, handler: nil))
+        //実際に表示させる
+        self.present(dialog, animated: true, completion: nil)
+        
+    }
+    
+    // モーションデータの取得（例としてコンソールへ出力）
+//    func getMotionData(deviceMotion:CMDeviceMotion) {
+//        print("attitudeX:", deviceMotion.attitude.pitch)
+//        print("attitudeY:", deviceMotion.attitude.roll)
+//        print("attitudeZ:", deviceMotion.attitude.yaw)
+//        print("gyroX:", deviceMotion.rotationRate.x)
+//        print("gyroY:", deviceMotion.rotationRate.y)
+//        print("gyroZ:", deviceMotion.rotationRate.z)
+//        print("gravityX:", deviceMotion.gravity.x)
+//        print("gravityY:", deviceMotion.gravity.y)
+//        print("gravityZ:", deviceMotion.gravity.z)
+//        print("accX:", deviceMotion.userAcceleration.x)
+//        print("accY:", deviceMotion.userAcceleration.y)
+//        print("accZ:", deviceMotion.userAcceleration.z)
+//    }
+    
+    
 }
--- AccelerationProphecyMagicApp/Controller/SettingsViewController.swift
+++ AccelerationProphecyMagicApp/Controller/SettingsViewController.swift
@@ -8,23 +8,21 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-    
-    @IBOutlet weak var frontImage: UIImageView!
-    @IBOutlet weak var rightImage: UIImageView!
-    @IBOutlet weak var leftImage: UIImageView!
-    @IBOutlet weak var backImage: UIImageView!
+class SettingsViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
     
     
-    var selectedImageString = String()
+    var turnDirectionStringArray = ["↑","→","↓","←"]
+    
+    var cardImageStringArray = ["Heart","Club","Diamond","Spade"]
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        frontImage.image = UIImage(named: "Heart")
-        rightImage.image = UIImage(named: "Club")
-        leftImage.image = UIImage(named: "Spade")
-        backImage.image = UIImage(named: "Diamond")
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .clear
         
     }
     
@@ -32,6 +30,36 @@ class SettingsViewController: UIViewController {
         
         dismiss(animated: true, completion: nil)
         
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return tableView.frame.size.height/4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.backgroundColor = .clear
+        
+        let turnDirectionLabel = cell.contentView.viewWithTag(1) as! UILabel
+        turnDirectionLabel.text = turnDirectionStringArray[indexPath.row]
+        turnDirectionLabel.textColor = .white
+        
+        
+        let cardImage = cell.contentView.viewWithTag(2) as! UIImageView
+        cardImage.image = UIImage(named: cardImageStringArray[indexPath.row])
+        cardImage.frame = CGRect(x: cardImage.frame.origin.x, y: cardImage.frame.origin.y, width: cardImage.frame.size.width, height: cell.frame.size.height * 0.8)
+        
+        return cell
     }
 
 }
