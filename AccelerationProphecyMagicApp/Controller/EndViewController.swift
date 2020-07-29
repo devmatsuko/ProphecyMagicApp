16dd61877d946d273d20374f236f0f08c3c7aa68
devmatsuko <devmatsuko@gmail.com>
2020-07-29 21:46:52 +0900

    20200729 iPhone11向け初版完成

---
+++ AccelerationProphecyMagicApp/Controller/EndViewController.swift
@@ -0,0 +1,88 @@
+//
+//  EndViewController.swift
+//  AccelerationProphecyMagicApp
+//
+//  Created by 小松真也 on 2020/07/28.
+//  Copyright © 2020 小松真也. All rights reserved.
+//
+
+import UIKit
+import CoreMotion
+
+class EndViewController: UIViewController {
+    
+    
+    @IBOutlet weak var endImage: UIImageView!
+    
+    @IBOutlet weak var backButton: UIButton!
+    
+    // MotionManager
+    let motionManager = CMMotionManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //トランプの画像を表示
+        self.endImage.image = UIImage(named: "Diamond")
+        
+        if self.motionManager.isAccelerometerAvailable {
+            // intervalの設定 [sec]
+            self.motionManager.accelerometerUpdateInterval = 0.2
+
+            // センサー値の取得開始
+            self.motionManager.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: {(motion:CMDeviceMotion?, error:Error?) in
+                
+                self.getMotionData(deviceMotion: motion!)
+                if (motion?.gravity.x)! >= 0.9 {
+                    //センサーをストップする
+                    self.motionManager.stopDeviceMotionUpdates()
+                    //トランプの画像を表示
+                    self.endImage.image = UIImage(named: "Spade")
+                }else if (motion?.gravity.x)! <= -0.9 {
+                    //センサーをストップする
+                    self.motionManager.stopDeviceMotionUpdates()
+                     //トランプの画像を表示
+                    self.endImage.image = UIImage(named: "Club")
+                }else if (motion?.gravity.y)! >= 0.9 {
+                    //センサーをストップする
+                    self.motionManager.stopDeviceMotionUpdates()
+                     //トランプの画像を表示
+                    self.endImage.image = UIImage(named: "Heart")
+                }else if (motion?.gravity.y)! <= -0.9 {
+                    //センサーをストップする
+                    self.motionManager.stopDeviceMotionUpdates()
+                }
+            })
+        }
+    }
+    
+    
+    @IBAction func backButton(_ sender: Any) {
+        let secondViewController = self.storyboard?.instantiateViewController(withIdentifier: "Top") as! ViewController
+        self.present(secondViewController, animated: true, completion: nil)
+    }
+    
+
+   //モーションデータの取得（例としてコンソールへ出力）
+    func getMotionData(deviceMotion:CMDeviceMotion) {
+        print("attitudeX:", deviceMotion.attitude.pitch)
+        print("attitudeY:", deviceMotion.attitude.roll)
+        print("attitudeZ:", deviceMotion.attitude.yaw)
+        print("gyroX:", deviceMotion.rotationRate.x)
+        print("gyroY:", deviceMotion.rotationRate.y)
+        print("gyroZ:", deviceMotion.rotationRate.z)
+        print("gravityX:", deviceMotion.gravity.x)
+        print("gravityY:", deviceMotion.gravity.y)
+        print("gravityZ:", deviceMotion.gravity.z)
+        print("accX:", deviceMotion.userAcceleration.x)
+        print("accY:", deviceMotion.userAcceleration.y)
+        print("accZ:", deviceMotion.userAcceleration.z)
+    }
+}
--- AccelerationProphecyMagicApp/Controller/PlayViewController.swift
+++ AccelerationProphecyMagicApp/Controller/PlayViewController.swift
@@ -21,19 +21,13 @@ class PlayViewController: UIViewController {
     // MotionManager
     let motionManager = CMMotionManager()
     
+    //Play Flag
+    var playFlag:Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        if motionManager.isAccelerometerAvailable {
-//           // intervalの設定 [sec]
-//           motionManager.accelerometerUpdateInterval = 0.2
-//
-//           // センサー値の取得開始
-//           motionManager.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: {(motion:CMDeviceMotion?, error:Error?) in
-//               self.getMotionData(deviceMotion: motion!)
-//
-//           })
-//       }
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -46,30 +40,67 @@ class PlayViewController: UIViewController {
     
     @IBAction func playStart(_ sender: Any) {
         
+        //PlayFlagをオンにする
+        playFlag = true
+        
         //アラートの作成
         let dialog = UIAlertController(title: nil, message: "スマートフォンの画面が下向きになるように置いてください。", preferredStyle: .alert)
-        //ボタンのタイトル
-        dialog.addAction(UIAlertAction(title: "Return to Top", style: .default, handler: nil))
-        //実際に表示させる
+        
+        //TOP画面へ戻るボタン
+        dialog.addAction(UIAlertAction(title: "Return to Top", style: .default, handler:{(action: UIAlertAction!) in
+            
+            //PlayFlagをオフにする
+            self.playFlag = false
+            
+            self.motionManager.stopDeviceMotionUpdates()
+            
+            //TOP画面へ遷移する
+            self.dismiss(animated: true, completion: nil)
+            
+        }))
+        
+        //実際にアラートを表示させる
         self.present(dialog, animated: true, completion: nil)
+        
+        if self.motionManager.isAccelerometerAvailable {
+            // intervalの設定 [sec]
+            self.motionManager.accelerometerUpdateInterval = 0.2
+
+            // センサー値の取得開始
+            self.motionManager.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: {(motion:CMDeviceMotion?, error:Error?) in
+                
+                 self.getMotionData(deviceMotion: motion!)
+                 if self.playFlag == true && (motion?.gravity.z)! >= 0.9 {
+                     
+                     //PlayFlagをオンにする
+                     self.playFlag = true
+                    
+                     //センサーをストップする
+                     self.motionManager.stopDeviceMotionUpdates()
+                    
+                     //End画面に遷移する
+                     self.performSegue(withIdentifier: "end", sender: nil)
+                 }
+            })
+        }
         
     }
     
-    // モーションデータの取得（例としてコンソールへ出力）
-//    func getMotionData(deviceMotion:CMDeviceMotion) {
-//        print("attitudeX:", deviceMotion.attitude.pitch)
-//        print("attitudeY:", deviceMotion.attitude.roll)
-//        print("attitudeZ:", deviceMotion.attitude.yaw)
-//        print("gyroX:", deviceMotion.rotationRate.x)
-//        print("gyroY:", deviceMotion.rotationRate.y)
-//        print("gyroZ:", deviceMotion.rotationRate.z)
-//        print("gravityX:", deviceMotion.gravity.x)
-//        print("gravityY:", deviceMotion.gravity.y)
-//        print("gravityZ:", deviceMotion.gravity.z)
-//        print("accX:", deviceMotion.userAcceleration.x)
-//        print("accY:", deviceMotion.userAcceleration.y)
-//        print("accZ:", deviceMotion.userAcceleration.z)
-//    }
+    //モーションデータの取得（例としてコンソールへ出力）
+    func getMotionData(deviceMotion:CMDeviceMotion) {
+        print("attitudeX:", deviceMotion.attitude.pitch)
+        print("attitudeY:", deviceMotion.attitude.roll)
+        print("attitudeZ:", deviceMotion.attitude.yaw)
+        print("gyroX:", deviceMotion.rotationRate.x)
+        print("gyroY:", deviceMotion.rotationRate.y)
+        print("gyroZ:", deviceMotion.rotationRate.z)
+        print("gravityX:", deviceMotion.gravity.x)
+        print("gravityY:", deviceMotion.gravity.y)
+        print("gravityZ:", deviceMotion.gravity.z)
+        print("accX:", deviceMotion.userAcceleration.x)
+        print("accY:", deviceMotion.userAcceleration.y)
+        print("accZ:", deviceMotion.userAcceleration.z)
+    }
     
     
 }
