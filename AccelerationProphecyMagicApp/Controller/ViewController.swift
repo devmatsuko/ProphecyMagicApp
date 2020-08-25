4cf2f64e5784b2e86ca562ee5fb5a831f3d1f29a
devmatsuko <devmatsuko@gmail.com>
2020-08-25 20:56:15 +0900

    [Update] TOPページにGoogleAdmobのバナー広告の実装

--- AccelerationProphecyMagicApp/Controller/ViewController.swift
+++ AccelerationProphecyMagicApp/Controller/ViewController.swift
@@ -7,15 +7,35 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,GADBannerViewDelegate {
+    
+    // AdMob ID を入れる(GitにPushするときは消す！！)
+    let AdMobID = ""
+    
+    @IBOutlet weak var bannerView: GADBannerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        //広告を表示する関数
+        showAdBanner()
+    }
+    
+    func showAdBanner(){
+        
+        bannerView.adUnitID = AdMobID
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+        
+    }
+    
 
 }
 
